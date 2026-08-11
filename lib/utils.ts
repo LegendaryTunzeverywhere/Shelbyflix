6ee8b3b876857a2108d5207fbfc77681010ce86d
Adewale Okunade Christopher <princecph4@gmail.com>
2026-08-11 10:47:22 +0100

    fixed upload progress syntax error

--- lib/utils.ts
+++ lib/utils.ts
@@ -108,6 +108,7 @@ export async function uploadToShelby(
       encryptedBlob,
       blobName,
       blobId,
+      uploaderAddress,
       (uploadProgress) => {
         onProgress?.({
           stage: 'uploading',
